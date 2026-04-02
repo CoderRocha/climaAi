8c3f3094b3ac4d8b5722a2138475eab6cc62b0e7
Guilherme <guilhermerochas0420@gmail.com>
2026-04-01 21:00:25 -0300

    init repository

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import WeatherApp from "./components/WeatherApp";
+import './components/WeatherApp.css'
+
+export default function App() {
+  return (
+    <div>
+      <WeatherApp />
+    </div>
+  )
+}
