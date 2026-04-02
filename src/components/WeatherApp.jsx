5ff55554345dd8cb162e1bbc7debd30efade758e
Guilherme <guilhermerochas0420@gmail.com>
2026-04-01 22:05:03 -0300

    feat: added customized weather backgrounds

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -43,9 +43,31 @@ export default function WeatherApp() {
         }
     }
 
+    const weatherImages = {
+        Clear: sunny,
+        Clouds: cloudy,
+        Rain: rainy,
+        Snow: snowy,
+        Haze: cloudy,
+        Mist: cloudy,
+    }
+
+    const weatherImage = data.weather ? weatherImages[data.weather[0].main] : null;
+
+    const backgroundImages = {
+        Clear: 'linear-gradient(to right, #f3b07c, #fcd283)',
+        Clouds: 'linear-gradient(to right, #57d6d4, #71eeec)',
+        Rain: 'linear-gradient(to right, #5bc8fb, #80eaff)',
+        Snow: 'linear-gradient(to right, #aff2ff, #fff)',
+        Haze: 'linear-gradient(to right, #57d6d4, #71eeec)',
+        Mist: 'linear-gradient(to right, #57d6d4, #71eeec)',
+    }
+
+    const backgroundImage = data.weather ? backgroundImages[data.weather[0].main] : null;
+
     return (
-        <div className="container">
-            <div className="weather-app">
+        <div className="container" style={{backgroundImage: backgroundImage}}>
+            <div className="weather-app" style={{backgroundImage: backgroundImage ? backgroundImage.replace("to right", "to top") : null}}>
                 <div className="search">
                     <div className="search-top">
                         <i className="fa-solid fa-location-dot"></i>
@@ -59,7 +81,7 @@ export default function WeatherApp() {
                     </div>
                 </div>
                 <div className="weather">
-                    <img src={sunny} alt="sunny" />
+                    <img src={weatherImage} alt="sunny" />
                     <div className="weather-type">{data.weather ? data.weather[0].main : null}</div>
                     <div className="temp">{data.main ? `${Math.floor(data.main.temp)}º` : null}</div>
                 </div>
